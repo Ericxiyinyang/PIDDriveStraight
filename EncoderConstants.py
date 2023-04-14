eb91f15fbaea9c60988b7643585cd170f3d83bb2
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-04-14 09:33:40 -0400

    Added new EncoderConstants file so programming work can go on without calculating the constants

---
+++ EncoderConstants.py
@@ -0,0 +1,2 @@
+class EncoderConstants:
+    distancePerTick = 40/100
--- drivetrain.py
+++ drivetrain.py
@@ -1,6 +1,7 @@
 import wpilib as wp
 from wpilib import Spark, Encoder
 import wpilib.drive as drive
+from EncoderConstants import EncoderConstants as EC
 
 class Drivetrain:
     def __init__(self):
@@ -8,6 +9,8 @@ class Drivetrain:
         self.rMotor = Spark(1)
         self.lEncoder = Encoder(4, 5)
         self.rEncoder = Encoder(6, 7)
+        self.lEncoder.setDistancePerPulse(EC.distancePerTick)
+        self.rEncoder.setDistancePerPulse(EC.distancePerTick)
         self.drivetrain = drive.DifferentialDrive(self.lMotor, self.rMotor)
 
     def move(self, forward, rotate):
