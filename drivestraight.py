52ab297378dc927cf01d56a4c8fb514490f850d5
Eric Yang <60325051+Ericxiyinyang@users.noreply.github.com>
2023-05-02 19:44:19 -0400

    Implemented two PID controllers for DriveStraight

--- drivestraight.py
+++ drivestraight.py
@@ -7,34 +7,44 @@ from autoroutine import AutoRoutine
 class StraightDriver(AutoRoutine):
     def __init__(self, drivetrain):
         self.drivetrain = drivetrain
-        self.kp = MC.PropRotationConstant
+        # self.kp = MC.PropRotationConstant
         self.goal = EC.intendedDistance
         self.tolerance = 3
-        self.pid_controller = PIDController(
+        self.dir_pid_controller = PIDController(
             20,
             0,
             0
         )
-        self.pid_controller.setSetpoint(0)
-        self.pid_controller.setTolerance(0.05)
-        self.pid_controller.setIntegratorRange(-.3, .3)
+        self.dir_pid_controller.setSetpoint(0)
+        self.dir_pid_controller.setTolerance(0.05)
+        self.dir_pid_controller.setIntegratorRange(-0.3, 0.3)
+        self.fwd_pid_controller = PIDController(
+            0.5,
+            0,
+            0
+        )
+        self.fwd_pid_controller.setSetpoint(self.goal)
+        self.fwd_pid_controller.setTolerance(0.05)
 
     def run(self):
 
         if self.drivetrain.getAvgDistanceTravelled() >= self.goal:
             print("STOPPED MOVING")
-            return
+            self.drivetrain.move(0, 0)
 
         lTravel = self.drivetrain.getLEncoderDistance()
         rTravel = self.drivetrain.getREncoderDistance()
+        avgDistance = self.drivetrain.getAvgDistanceTravelled()
         diff = lTravel - rTravel
-        rotate = self.pid_controller.calculate(diff)
-        if self.pid_controller.atSetpoint():
+        rotate = self.dir_pid_controller.calculate(diff)
+        forward = self.fwd_pid_controller.calculate(avgDistance)
+
+        # print(f"Left traveled:{lTravel}, Right traveled:{rTravel}, Avg traveled:{self.drivetrain.getAvgDistanceTravelled()}")
+        if self.dir_pid_controller.atSetpoint():
             rotate = 0
-        print(f"Left traveled:{lTravel}, Right traveled:{rTravel}, Avg traveled:{self.drivetrain.getAvgDistanceTravelled()}")
-        if lTravel == rTravel:
-            self.drivetrain.move(0, MC.forwardAmount)
+        elif self.fwd_pid_controller.atSetpoint():
+            self.drivetrain.move(0, 0)
         else:
-            rotate = diff * self.kp
-            fwd = MC.forwardAmount
-            self.drivetrain.move(rotate, fwd)
+            # rotate = diff * self.kp
+            self.drivetrain.move(rotate, forward)
+        print(f"{forward=}, {rotate=}, distance: {self.drivetrain.getAvgDistanceTravelled()}, difference: {diff}")
